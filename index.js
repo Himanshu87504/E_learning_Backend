5792c563df7e6f67f61706edc0b4a34dbaa57fd8
Himanshu87504 <165474672+Himanshu87504@users.noreply.github.com>
2025-09-03 08:16:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",  // frontend origin
+  origin: "https://e-learning-front-neam1nqb8-himanshu87504s-projects.vercel.app",  // frontend origin
   credentials: true,                // allow cookies/session
 }));
 
