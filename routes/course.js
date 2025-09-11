a1989c517ea6f4cc82ee807890a9cd59415c594c
Himanshu87504 <165474672+Himanshu87504@users.noreply.github.com>
2025-09-11 17:08:51 +0530

    Update course.js

--- routes/course.js
+++ routes/course.js
@@ -17,7 +17,9 @@ router.get("/course/:id", getSingleCourse);
 router.get("/lectures/:id", isAuth, fetchLectures);
 router.get("/lecture/:id", isAuth, fetchLecture);
 router.get("/mycourse", isAuth, getMyCourses);
-router.post("/course/checkout/:id", isAuth, checkout);
 router.post("/verification/:id", isAuth, paymentVerification);
+
+router.post("/checkout/:id", isAuth, checkout);
+
 
 export default router;
